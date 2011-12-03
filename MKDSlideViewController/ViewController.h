0e5d8e99e10ee273cd462ed7c4733cd6b8d6e41b
Marcel Dierkes <marcel.dierkes@gmail.com>
2011-12-03 23:00:16 +0100

    Initial Commit

---
+++ MKDSlideViewController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MKDSlideViewController
+//
+//  Created by Marcel Dierkes on 03.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
